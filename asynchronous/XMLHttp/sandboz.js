fc9e519047d9e92ba5877de8d64be17f26e683ef
eduardonk9999 <thiago.novak7@gmail.com>
2021-06-28 11:05:05 -0300

    Add, status code

--- asynchronous/XMLHttp/sandboz.js
+++ asynchronous/XMLHttp/sandboz.js
@@ -2,7 +2,7 @@ const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange', () => {
   if(request.readyState === 4) {
-    console.log(request.responseText);
+    console.log(request.responseText, request.responseText);
   }
 });
 
---
+++ asynchronous/statuscodes.js
@@ -0,0 +1,4 @@
+/*
+No Status Codes, é onde geralmente vamos encontar o que buscamos da requisição
+O nome do que temos que busca dentro do objeto é ResponseText
+*/
