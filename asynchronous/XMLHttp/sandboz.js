95190dc242c705f75e5c179503904f9f211993bf
eduardonk9999 <thiago.novak7@gmail.com>
2021-06-29 09:10:15 -0300

    Add, aula final status code

--- asynchronous/XMLHttp/sandboz.js
+++ asynchronous/XMLHttp/sandboz.js
@@ -1,8 +1,10 @@
 const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange', () => {
-  if(request.readyState === 4) {
+  if(request.readyState === 4 && request.status === 200) {
     console.log(request.responseText, request.responseText);
+  } else if(request.readyState === 4 ) {
+    console.log('could not fetch the data')
   }
 });
 
